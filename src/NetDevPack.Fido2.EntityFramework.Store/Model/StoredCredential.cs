a03d0c0aec7d7c54ce18ffb278a23023f8da3d18
Bruno Brito <bhdebrito@gmail.com>
2022-08-25 13:42:25 -0300

    fix: data annotations

--- src/NetDevPack.Fido2.EntityFramework.Store/Model/StoredCredential.cs
+++ src/NetDevPack.Fido2.EntityFramework.Store/Model/StoredCredential.cs
@@ -1,23 +1,35 @@
 ﻿using Fido2NetLib.Objects;
+using System.ComponentModel.DataAnnotations;
 
 namespace NetDevPack.Fido2.EntityFramework.Store.Model;
 
 public class StoredCredential
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public string Username { get; set; }
-    public byte[] UserId { get; set; }
+
+    public byte[]? UserId { get; set; }
+    
+    [Required]
     public byte[] PublicKey { get; set; }
+    
+    [Required]
+    public byte[] PublicKeyId { get; set; }
+
+    [Required]
     public byte[] UserHandle { get; set; }
+
     public uint SignatureCounter { get; set; }
-    public string CredType { get; set; }
+    public string? CredType { get; set; }
     public DateTime RegDate { get; set; }
     public Guid AaGuid { get; set; }
 
-
-    public byte[] PublicKeyId { get; set; }
+    
     public PublicKeyCredentialType? Type { get; set; }
-    public string Transports { get; set; }
+    public string? Transports { get; set; }
 
     public StoredCredential SetUsername(string username)
     {
